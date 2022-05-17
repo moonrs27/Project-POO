5ace84f1207b0ae20e636c28c901aa13e48c9124
ivan <ivanmoreno405@gmail.com>
2022-05-17 06:53:00 -0500

    example comments

--- TvDemo/src/PBotones/BotonApagado.java
+++ TvDemo/src/PBotones/BotonApagado.java
@@ -35,11 +35,27 @@ public class BotonApagado extends Boton {
   // Other methods
   //
 
+/**
+* Cruz Moreno Carlos Ivan 
+* Implementacion del patron comando 
+* desacoplamineto del metodo execute hacia el metodo apagarTv 
+*/
+
     @Override
     public void execute() {
         apagarTv();
     }
 
+/**
+* Cruz Moreno Carlos Ivan 
+* Implementacion de apagarTv
+* Llamando apagar de la referencia Tv a la que pertenece el BotonApagado
+* BotonApagado mantiene una referencia local a Tv 
+* Tv se compone de un BotonApagado
+* new Clase(){
+*		//En este bloque se puede ver todos los atributos y metodos de BotonApagado
+*	}
+*/
     private void apagarTv() {
         this.tv.apagar();
     }
