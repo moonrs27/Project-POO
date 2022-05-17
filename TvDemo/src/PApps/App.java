eee6ad0f70104816744e49a9ecf5e5ac8024cb91
Alan045 <39870260+Alan045@users.noreply.github.com>
2022-05-16 21:42:34 -0500

    Add files via upload

--- TvDemo/src/PApps/App.java
+++ TvDemo/src/PApps/App.java
@@ -1,33 +1,49 @@
-package PApps;
-
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_tvdemo;
 
 /**
- * Class App
+ *
+ * @author alanm
  */
-public class App {
+public class App extends Thread{
+    private String nombreApp;
+    private boolean Estado;
 
-  //
-  // Fields
-  //
+    public String getNombreApp() {
+        return nombreApp;
+    }
 
-  
-  //
-  // Constructors
-  //
-  //Herrera improvement public App () { };
-  
-  //
-  // Methods
-  //
+    public void setNombreApp(String nombreApp) {
+        this.nombreApp = nombreApp;
+    }
 
+    public boolean isEstado() {
+        return Estado;
+    }
 
-  //
-  // Accessor methods
-  //
-
-  //
-  // Other methods
-  //
-
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    }
+    
+    public void Abrir(){
+        
+        System.out.println("Llego a las APPs");
+        /*AppStreaming Stream = new AppStreaming();
+        AppMusica Music = new AppMusica();
+        
+        if(this.Estado == false && this.nombreApp == "Stream"){
+         Stream.start();
+       }else{
+         Music.start();
+         
+        
+        } */  
+        
+    }
+    
+    
 }
