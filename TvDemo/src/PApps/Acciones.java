d2e65ff38c8d4179d1ee1b0dd113f7137f8660f4
Alan045 <39870260+Alan045@users.noreply.github.com>
2022-05-23 23:25:29 -0500

    Add files via upload

---
+++ TvDemo/src/PApps/Acciones.java
@@ -0,0 +1,39 @@
+
+package PApps;
+
+/**
+ *
+ * package Acciones
+ */
+public interface Acciones {
+    
+  //
+  // Fields
+  //
+    
+
+    
+  //
+  // Constructors
+  //
+  //Herrera improvement 
+
+    
+  
+  //
+  // Methods
+  //
+
+    
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+    public void abrir();
+    public void cerrar();
+    
+}
