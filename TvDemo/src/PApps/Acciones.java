bbc6e824f04983cab7891f93e2de0774a2ee6061
moonrs27 <104441686+moonrs27@users.noreply.github.com>
2022-05-25 21:51:11 -0500

    Update Acciones.java

--- TvDemo/src/PApps/Acciones.java
+++ TvDemo/src/PApps/Acciones.java
@@ -1,6 +1,11 @@
 
 package PApps;
 
+/*
+MONTSERRAT ROJAS SANTILLAN
+paquete de la aplicacion de transmision y musica
+*/
+
 /**
  *
  * package Acciones
@@ -11,8 +16,13 @@ public interface Acciones {
   // Fields
   //
     
+/*
+MONTSERRAT ROJAS SANTILLAN
+llamamos a la interfaz acciones que se encargara de realizar las operaciones de los campos de la aplicacion
+los constructores para los objetos, los metodos para reproducir la pestaña con los botones tanto para la aplicacion de 
+musica como para la de transmision y los accesos remotos para las clases abstractas
 
-    
+*/    
   //
   // Constructors
   //
@@ -35,5 +45,10 @@ public interface Acciones {
   //
     public void abrir();
     public void cerrar();
+    
+    /*
+MONTSERRAT ROJAS SANTILLAN
+finalmente llamamos los metodos para abrir y cerrar las aplicaciones
+*/ 
     
 }
